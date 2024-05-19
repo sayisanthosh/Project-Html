8f3d9d72c117c7d8504669557691787c9a93448d
sayisanthosh <160519187+sayisanthosh@users.noreply.github.com>
2024-05-19 14:30:04 +0530

    Add files via upload
    This setup provides a simple yet functional drum kit that users can interact with both through clicking buttons and pressing keys on their image.

---
+++ DRUMKIT/index.js
@@ -0,0 +1,26 @@
+const kits = ["CRASH", "KICK", "SNARE", "TOM"];
+
+const containerEl = document.querySelector(".container");
+
+kits.forEach((kit) => {
+  const btnEl = document.createElement("button");
+  btnEl.classList.add("btn");
+  btnEl.innerText = kit;
+  btnEl.style.backgroundImage = "url(images/" + kit + ".png)";
+  containerEl.appendChild(btnEl);
+  const audioEl = document.createElement("audio");
+  audioEl.src = "sounds/" + kit + ".wav";
+  containerEl.appendChild(audioEl);
+  btnEl.addEventListener("click", () => {
+    audioEl.play();
+  });
+  window.addEventListener("keydown", (event) => {
+    if (event.key === kit.slice(0, 1)) {
+      audioEl.play();
+      btnEl.style.transform = "scale(.9)";
+      setTimeout(() => {
+        btnEl.style.transform = "scale(1)";
+      }, 100);
+    }
+  });
+});
