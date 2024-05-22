651828b403bdebd5804cac16bad02abc7c5b244a
sayisanthosh <160519187+sayisanthosh@users.noreply.github.com>
2024-05-22 16:48:23 +0530

    Add files via upload

---
+++ Weather-App(html)/Weather-App.js
@@ -0,0 +1,67 @@
+let weather = {
+  apiKey: "67b92f0af5416edbfe58458f502b0a31",
+  fetchWeather: function (city) {
+    document.querySelector(".weather").classList.add("loading");
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data))
+      .catch((error) => {
+        alert(error.message);
+        this.clearWeather();
+      });
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "�C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  clearWeather: function () {
+    document.querySelector(".city").innerText = "";
+    document.querySelector(".icon").src = "";
+    document.querySelector(".description").innerText = "";
+    document.querySelector(".temp").innerText = "";
+    document.querySelector(".humidity").innerText = "";
+    document.querySelector(".wind").innerText = "";
+    document.querySelector(".weather").classList.add("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Chennai");
